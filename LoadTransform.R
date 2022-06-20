dfee370a1424bfcf5d64deb834f85ed641852f23
dikbrown <dikbrown@gmail.com>
2022-06-19 23:41:09 -0400

    More transform code in LoadTransform.R modeling code in RF.R Submission to Kaggle in sample_submission.csv

--- LoadTransform.R
+++ LoadTransform.R
@@ -1,35 +1,74 @@
 library(dplyr)
+seed <- 42
 
 #Read in train and test sets
 fulltrain <- read.csv("./data/train.csv")
 test <- read.csv("./data/test.csv")
 
-#split training data into train and validate sets
-nobs <- nrow(fulltrain)
-validate_idx <- sample(nobs, 0.2*nobs) #use 20% for validation
-train <- fulltrain[-validate_idx,]
-validate <- fulltrain[validate_idx,]
+#####################
+##### Transform #####
+#####################
 
 #separate out cabin info into deck/num/side
-train$deck <- substr(train$Cabin, 1, 1)
-train$room_num <- substr(train$Cabin, 3, 3)
-train$side <- substr(train$Cabin, 5, 5)
-train <- train[,-4] #remove Cabin column
+fulltrain$deck <- stringr::word(fulltrain$Cabin, 1, sep = "/")
+fulltrain$room_num <- stringr::word(fulltrain$Cabin, 2, sep = "/")
+fulltrain$side <- stringr::word(fulltrain$Cabin, 3, sep = "/")
+fulltrain <- fulltrain[,-4] #remove Cabin column
 
-test$deck <- substr(test$Cabin, 1, 1)
-test$room_num <- substr(test$Cabin, 3, 3)
-test$side <- substr(test$Cabin, 5, 5)
+test$deck <- stringr::word(test$Cabin, 1, sep = "/")
+test$room_num <- stringr::word(test$Cabin, 2, sep = "/")
+test$side <- stringr::word(test$Cabin, 3, sep = "/")
 test <- test[,-4] # remove Cabin column
 
 
 #pull group number from id
-train$group <- factor(substr(train$PassengerId, 1, 4))
+fulltrain$group <- factor(substr(fulltrain$PassengerId, 1, 4))
 test$group <- factor(substr(test$PassengerId, 1, 4))
 
 #Calculate group size
-group_sizes <- train %>%
-                group_by(group) %>%
-                summarize(grp_size = n())
+train_group_sizes <- fulltrain %>%
+  group_by(group) %>%
+  summarize(grp_size = n())
 
-#add group size to dataset
-train2 <- inner_join(train, group_sizes)
+test_group_sizes <- test %>%
+  group_by(group) %>%
+  summarize(grp_size = n())
+
+#add group size to datasets
+fulltrain <- inner_join(fulltrain, train_group_sizes)
+test <- inner_join(test, test_group_sizes)
+
+############################################
+## Convert categoric variables to factors ##
+############################################
+fulltrain$HomePlanet <- factor(fulltrain$HomePlanet)
+fulltrain$CryoSleep <- factor(fulltrain$CryoSleep)
+fulltrain$Destination <- factor(fulltrain$Destination)
+fulltrain$VIP <- factor(fulltrain$VIP)
+fulltrain$deck <- factor(fulltrain$deck)
+fulltrain$side <- factor(fulltrain$side)
+fulltrain$Transported <- factor(fulltrain$Transported)
+fulltrain$room_num <- as.numeric(fulltrain$room_num)
+
+test$HomePlanet <- factor(test$HomePlanet)
+test$CryoSleep <- factor(test$CryoSleep)
+test$Destination <- factor(test$Destination)
+test$VIP <- factor(test$VIP)
+test$deck <- factor(test$deck)
+test$side <- factor(test$side)
+test$room_num <- as.numeric(test$room_num)
+
+############################
+## Impute NAs in test set ##
+############################
+
+test2 <- na.roughfix(test[,-c(1,12)])
+
+########################################################
+### split training data into train and validate sets ###
+########################################################
+set.seed(seed)
+nobs <- nrow(fulltrain)
+validate_idx <- sample(nobs, 0.2*nobs) #use 20% for validation
+train <- fulltrain[-validate_idx,]
+validate <- fulltrain[validate_idx,]
--- RF.R
+++ RF.R
@@ -1,11 +1,12 @@
 library(randomForest)
 library(rpart)
+###results of modeling @ https://docs.google.com/spreadsheets/d/1Yviru268eUQLLOVwfKBhv0N97o0-hcEtdG8N7BKmOTg/edit#gid=0
 
 seed <- 42
 
 set.seed(seed)
-ind_var <- c(Passenger)
-rfset <- train2[,c(2, #HomePlanet 
+ 
+rfset <- train[,c(2, #HomePlanet 
                   3, #CryoSleep
                   4, #Destination
                   5, #Age
@@ -21,11 +22,37 @@ rfset <- train2[,c(2, #HomePlanet
                   18, #grp_size - number of members in group
                   13 #Transported - Target
                   )]
-
+set.seed(seed)
 forest1 <- randomForest(factor(Transported) ~ .,
                         data = rfset,
                         ntree = 500,
                         mtry = 3,
                         importance = TRUE,
-                        na.action = na.omit,
+                        na.action = na.roughfix,
                         replace = FALSE)
+
+rfpred <- predict(forest1, newdata = test2)
+
+############
+rfoutput <- data.frame(rfpred)
+
+output <- data.frame(PassengerID = test$PassengerId, Transported = rfoutput$rfpred)
+
+write.csv(output, "./sample_submission.csv", row.names = FALSE, quote = FALSE)
+
+###
+### Summary:
+###
+###   Split training data 80/20 into training and validation sets
+###   Created 12 models on training data
+###     Three sets of four, using varying number of trees and variables
+###     Each set of models used a different set of independent variables
+###     First set used all variables.
+###     Second set removed VIP and side because of their low importance in first model
+###     Third set also removed Destination, room number, and group size
+###   From each set, chose the best-performing model and ran on the validation set
+###   Chose the model that gave the best performance on the validation set. As it turns 
+###       out, the best model was the first one I ran.
+###
+###   Results from model testing can be found at https://bit.ly/3Qv8eyH
+###
